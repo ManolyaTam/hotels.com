ed36ddcab5782cc047dfe182837951430481b9e7
Manolya Tamimi <tamimimanolya@gmail.com>
2023-12-13 21:08:52 +0200

    TABP-11: creating date range picker

---
+++ src/stories/DateRangePicker.stories.jsx
@@ -0,0 +1,25 @@
+import DateRangePicker from "../components/DateRangePicker";
+import dayjs from "dayjs";
+
+const DateRangePickerStory = {
+  component: DateRangePicker,
+  title: "Date Picker",
+  tags: ["autodocs"],
+};
+
+const today = dayjs();
+
+export const Default = {
+  args: {},
+};
+
+export const withGivenDate = {
+  args: {
+    defaultDate: {
+      start: today.add(10, "day"),
+      end: today.add(14, "day"),
+    },
+  },
+};
+
+export default DateRangePickerStory;
