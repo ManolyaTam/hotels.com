d3a61c79eb9675ce1293eed44c03904fb83e6aed
Manolya Tamimi <tamimimanolya@gmail.com>
2023-12-20 19:44:03 +0200

    TABP-19: creating featured deals section

--- src/pages/UHomePage.jsx
+++ src/pages/UHomePage.jsx
@@ -1,14 +1,36 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import getFeatured from "../services/getFeatured";
+import HotelCard from "../components/HotelCard";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
 
 const UHomePage = () => {
+  const [featured, setFeatured] = useState([]);
   useEffect(() => {
-    getFeatured();
+    const loadFeatured = async () => {
+      const result = await getFeatured();
+      setFeatured(result);
+    };
+    loadFeatured();
   }, []);
 
   return (
     <div>
-      <h3>User Home Page</h3>
+      <Typography variant="h6">featured deals</Typography>
+      <Box style={{ display: "flex", flexWrap: "wrap" }}>
+        {featured.map((item, index) => (
+          <div key={"featured-" + index} style={{ margin: 10 }}>
+            <HotelCard
+              {...item}
+              imgUrl={item.roomPhotoUrl}
+              price={item.finalPrice}
+              city={item.cityName}
+              rating={item.hotelStarRating}
+              originalPrice={item.originalRoomPrice}
+            />
+          </div>
+        ))}
+      </Box>
     </div>
   );
 };
