63f76fb3f7bc638e2c93e6611770974a176aa594
Manolya Tamimi <tamimimanolya@gmail.com>
2023-12-27 12:34:31 +0200

    TABP-28: fixing marker

--- src/pages/Hotel/Map.jsx
+++ src/pages/Hotel/Map.jsx
@@ -2,16 +2,22 @@ import { MapContainer } from "react-leaflet/MapContainer";
 import { TileLayer } from "react-leaflet/TileLayer";
 import { Marker } from "react-leaflet/Marker";
 import "./hotel.css";
+import { icon } from "leaflet";
+
+const marker = icon({
+  iconUrl: "/marker.png",
+  iconSize: [54, 54],
+});
 
 function Map({ xpos, ypos }) {
   const position = [xpos, ypos];
   return (
-    <MapContainer center={position} zoom={17} scrollWheelZoom={false}>
+    <MapContainer center={position} zoom={17} scrollWheelZoom={true}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={position}></Marker>
+      <Marker position={position} icon={marker} />
     </MapContainer>
   );
 }
