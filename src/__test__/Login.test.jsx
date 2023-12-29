291295b504bfc384124743477db5bd0d5117087a
Manolya Tamimi <tamimimanolya@gmail.com>
2023-12-29 10:23:25 +0200

    TABP-4: adding 2 more login tests (successfull login and invalid username/password)

--- src/__test__/Login.test.jsx
+++ src/__test__/Login.test.jsx
@@ -49,4 +49,61 @@ describe("login page", () => {
       expect(screen.getByText(/password is required/i)).toBeInTheDocument();
     });
   });
+
+  test("show message if username/password combination is invalid", async () => {
+    render(
+      <BrowserRouter>
+        <UserProvider>
+          <MessageProvider>
+            <Login />
+          </MessageProvider>
+        </UserProvider>
+      </BrowserRouter>,
+    );
+
+    const usernameField = screen.getByRole("textbox", { name: /username/i });
+    const passwordField = screen.getByLabelText(/password/i);
+    const loginButton = screen.getByRole("button", { name: /login/i });
+
+    userEvent.type(usernameField, "x");
+    userEvent.type(passwordField, "y");
+
+    userEvent.click(loginButton);
+
+    await waitFor(() => {
+      expect(
+        screen.getByText(
+          /Username\/Password combination is invalid, please try again/i,
+        ),
+      ).toBeInTheDocument();
+    });
+  });
+  test("successfull login with welcome message and routing", async () => {
+    render(
+      <BrowserRouter>
+        <UserProvider>
+          <MessageProvider>
+            <Login />
+          </MessageProvider>
+        </UserProvider>
+      </BrowserRouter>,
+    );
+
+    const usernameField = screen.getByRole("textbox", { name: /username/i });
+    const passwordField = screen.getByLabelText(/password/i);
+    const loginButton = screen.getByRole("button", { name: /login/i });
+
+    userEvent.type(usernameField, "user");
+    userEvent.type(passwordField, "user");
+
+    userEvent.click(loginButton);
+
+    await waitFor(() => {
+      expect(screen.getByText(/welcome back,/i)).toBeInTheDocument();
+    });
+
+    await waitFor(() => {
+      expect(window.location.pathname).toEqual("/home");
+    });
+  });
 });
