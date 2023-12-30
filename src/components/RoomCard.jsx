ad31956e796fe32237ab09c8fa0d5947b4778f3e
Manolya Tamimi <tamimimanolya@gmail.com>
2023-12-30 12:28:34 +0200

    TABP-32: adding services to room card

--- src/components/RoomCard.jsx
+++ src/components/RoomCard.jsx
@@ -1,16 +1,18 @@
 import {
   Card,
-  CardActionArea,
   CardContent,
   CardMedia,
   Box,
   Typography,
+  Container,
 } from "@mui/material";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
 import Button from "./Button";
 import PersonIcon from "@mui/icons-material/Person";
+import { useState } from "react";
 
 const RoomCard = ({
-  onClick,
   imgUrl,
   alt,
   roomType,
@@ -20,18 +22,24 @@ const RoomCard = ({
   roomNumber,
   roomAmenities,
 }) => {
+  const [viewServices, setViewServices] = useState(false);
   return (
     <Card sx={{ maxheight: "140px", display: "flex" }}>
-      <CardActionArea onClick={onClick} sx={{ maxWidth: "200px" }}>
-        <CardMedia component="img" image={imgUrl} alt={alt} />
-      </CardActionArea>
+      <Box sx={{ maxWidth: "300px" }}>
+        <CardMedia
+          component="img"
+          image={imgUrl}
+          sx={{ objectFit: "contain" }}
+          alt={alt}
+        />
+      </Box>
       <CardContent
         sx={{ display: "flex", flexDirection: "column", width: "100%" }}
       >
         <Typography fontSize={12} color="text.secondary">
           Room Number: <b>{roomNumber}</b>
         </Typography>
-        <Box display="flex" alignItems="center" justifyContent="space-between">
+        <Box>
           <Typography fontSize={12} color="text.secondary">
             Type: <b>{roomType}</b>
           </Typography>
@@ -44,6 +52,49 @@ const RoomCard = ({
             ))}
           </Box>
         </Box>
+        <Box className="master-detail">
+          <Box
+            style={{
+              border: "1px solid lightgrey",
+              padding: "5px",
+              margin: "2px",
+              borderRadius: "4px",
+            }}
+          >
+            <Box display="flex" alignItems="center">
+              <Button
+                style={{ padding: 0, width: "25px" }}
+                variant="text"
+                label={
+                  viewServices ? (
+                    <KeyboardArrowDownIcon />
+                  ) : (
+                    <KeyboardArrowRightIcon />
+                  )
+                }
+                onClick={() => setViewServices((viewServices) => !viewServices)}
+              />
+              <Typography>Services</Typography>
+            </Box>
+            {viewServices && (
+              <Box>
+                {roomAmenities.map((service, index) => (
+                  <Box key={index}>
+                    <Container>
+                      <Typography fontSize={13}>
+                        <b>{service.name}</b>
+                      </Typography>
+                      <Typography fontSize={12}>
+                        {service.description}
+                      </Typography>
+                    </Container>
+                  </Box>
+                ))}
+              </Box>
+            )}
+          </Box>
+        </Box>
+
         <Box
           style={{
             display: "flex",
