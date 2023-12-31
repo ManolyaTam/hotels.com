56983c9b87991e498f27c6a1891f40abe565f068
Manolya Tamimi <tamimimanolya@gmail.com>
2023-12-31 11:54:19 +0200

    TABP-12: rendering rooms in search page using static data

---
+++ src/components/DetailedRoomCard.jsx
@@ -0,0 +1,160 @@
+import {
+  Card,
+  Grid,
+  CardContent,
+  CardMedia,
+  Box,
+  Typography,
+  Container,
+  Chip,
+} from "@mui/material";
+import Button from "./Button";
+import PersonIcon from "@mui/icons-material/Person";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+
+const DetailedRoomCard = ({
+  imgUrl,
+  alt,
+  roomType,
+  adults,
+  children,
+  price,
+  roomNumber,
+  roomAmenities,
+  onClick,
+  showLinktoHotel,
+  hotelId,
+  hotelName,
+  rating,
+  discount,
+  city,
+}) => {
+  const navigate = useNavigate();
+  const [viewServices, setViewServices] = useState(true);
+
+  return (
+    <Card sx={{ padding: "5px", border: "1px solid lightgrey" }}>
+      <Grid container>
+        {/* First Column */}
+        <Grid item xs={12} md={4}>
+          <CardMedia
+            component="img"
+            image={imgUrl}
+            sx={{ objectFit: "contain" }}
+            alt={alt}
+          />
+        </Grid>
+
+        {/* Second Column */}
+        <Grid item xs={12} md={3}>
+          <CardContent sx={{ display: "flex", flexDirection: "column" }}>
+            <Typography fontSize={15} color="text.secondary">
+              {" "}
+              <b>
+                {hotelName}, {city}
+              </b>
+            </Typography>
+            <Typography fontSize={12} color="text.secondary">
+              Room Type: <b>{roomType}</b>
+            </Typography>
+            <Box>
+              {Array.from({ length: adults }).map((_, i) => (
+                <PersonIcon sx={{ fontSize: "20px" }} key={i} />
+              ))}
+              {Array.from({ length: children }).map((_, i) => (
+                <PersonIcon sx={{ fontSize: "15px" }} key={i} />
+              ))}
+            </Box>
+            {showLinktoHotel && (
+              <Button
+                label="View Hotel"
+                onClick={() => navigate(`/hotel/${hotelId}`)}
+              />
+            )}
+          </CardContent>
+        </Grid>
+        {/* Third Column */}
+        <Grid item xs={12} md={5}>
+          <CardContent>
+            <Box
+              className="master-detail"
+              style={{
+                border: "1px solid lightgrey",
+                padding: "5px",
+                margin: "2px",
+                borderRadius: "4px",
+              }}
+            >
+              <Box>
+                <Box display="flex" alignItems="center">
+                  {!showLinktoHotel && (
+                    <Button
+                      style={{ padding: 0, width: "25px" }}
+                      variant="text"
+                      label={viewServices ? "▶" : "▼"}
+                      onClick={() =>
+                        setViewServices((viewServices) => !viewServices)
+                      }
+                    />
+                  )}
+                  <Typography>Services</Typography>
+                </Box>
+              </Box>
+              {(!viewServices || showLinktoHotel) && (
+                <Grid>
+                  {roomAmenities.map((service, index) => (
+                    <Box key={index}>
+                      <Container>
+                        <Typography fontSize={13}>
+                          <b>{service.name}</b>
+                        </Typography>
+                        <Typography fontSize={12}>
+                          {service.description}
+                        </Typography>
+                      </Container>
+                    </Box>
+                  ))}
+                </Grid>
+              )}
+            </Box>
+
+            <Box
+              style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "space-between",
+                marginTop: "10px",
+              }}
+            >
+              <Box>
+                <Typography
+                  color="success.main"
+                  style={{ fontWeight: "bold", display: "block" }}
+                  component="span"
+                >
+                  ${price}
+                </Typography>
+                {discount && (
+                  <Chip
+                    color="success"
+                    size="small"
+                    label={`Get ${discount * 100} % off`}
+                  />
+                )}
+              </Box>
+              <Button
+                onClick={() => {
+                  navigate(`/hotel/${hotelId}`);
+                }}
+                label="View More"
+              />
+            </Box>
+          </CardContent>
+        </Grid>
+      </Grid>
+    </Card>
+  );
+};
+
+export default DetailedRoomCard;
--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -1,13 +1,37 @@
-import { Box } from "@mui/material";
+import { Grid, Box } from "@mui/material";
 import SearchForm from "../../components/SearchForm";
 import useParam from "../../hooks/useParam";
+import DetailedRoomCard from "../../components/DetailedRoomCard";
+import { searchResults } from "./searchResults";
 
+// Each hotel's entry to include a thumbnail, name, star rating, price per night, and a brief description.z
 const Search = () => {
   const { params } = useParam();
+
   return (
-    <Box>
+    <>
       <SearchForm initialValues={params} />
-    </Box>
+      <Grid container>
+        <Grid item xs={12} sm={3}>
+          hello world
+        </Grid>
+        <Grid item xs={12} sm={9}>
+          {searchResults.map((item, index) => (
+            <Box style={{ padding: 5 }}>
+              <DetailedRoomCard
+                key={index}
+                {...searchResults[0]}
+                imgUrl={item.roomPhotoUrl}
+                price={item.roomPrice} // dont forget the discount
+                rating={item.starRating}
+                city={item.cityName}
+                roomAmenities={item.amenities}
+              />
+            </Box>
+          ))}
+        </Grid>
+      </Grid>
+    </>
   );
 };
 
