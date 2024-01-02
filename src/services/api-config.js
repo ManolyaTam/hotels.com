cd04de1db1798b3d893c16139136261ff743a70b
Manolya Tamimi <tamimimanolya@gmail.com>
2024-01-02 19:27:48 +0200

    TABP-25: fixing date format functions

--- src/services/api-config.js
+++ src/services/api-config.js
@@ -1,7 +1,13 @@
 const BASE_URL =
   "https://app-hotel-reservation-webapi-uae-dev-001.azurewebsites.net";
 
-const formatDate = (date) => new Date(date).toISOString().split("T")[0];
-const formatDateTime = (dateTime) => new Date(dateTime).toISOString();
+const formatDate = (date) => {
+  date = parseInt(date, 10);
+  return new Date(date).toISOString().split("T")[0];
+};
+const formatDateTime = (dateTime) => {
+  dateTime = parseInt(dateTime, 10);
+  new Date(dateTime).toISOString();
+};
 
 export { BASE_URL, formatDate, formatDateTime };
