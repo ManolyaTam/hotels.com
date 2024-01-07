5db52cb9060d909b47e41d617dc32dd331a6a2a6
Manolya Tamimi <tamimimanolya@gmail.com>
2024-01-07 22:21:50 +0200

    TABP-35: creating a seperate card for recently visited hotels and adding min and max prices

---
+++ src/components/RecentHotelCard.jsx
@@ -0,0 +1,66 @@
+import {
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Box,
+  Typography,
+  Rating,
+} from "@mui/material";
+import Button from "./Button";
+import { useNavigate } from "react-router-dom";
+
+const RecentHotelCard = ({
+  imgUrl,
+  alt,
+  rating,
+  hotelName,
+  city,
+  priceMin,
+  priceMax,
+  date,
+  hotelId,
+}) => {
+  const dateTime = new Date(date);
+  const navigate = useNavigate();
+  return (
+    <Card sx={{ maxWidth: 350 }}>
+      <CardActionArea>
+        <CardMedia component="img" height="140" image={imgUrl} alt={alt} />
+      </CardActionArea>
+      <CardContent>
+        <Typography fontSize={15}>
+          {hotelName}, {city}
+        </Typography>
+
+        <Rating value={rating} size="small" precision={0.25} readOnly />
+        <Typography fontSize={12} color="text.secondary">
+          {dateTime?.toLocaleDateString()} at{" "}
+          {dateTime?.toLocaleTimeString([], {
+            hour: "2-digit",
+            minute: "2-digit",
+          })}
+        </Typography>
+
+        <Box display="block">
+          <Typography
+            color="success.main"
+            style={{ fontWeight: "bold" }}
+            component="span"
+          >
+            ${priceMin} - ${priceMax}
+          </Typography>
+        </Box>
+
+        <Button
+          label="See details"
+          onClick={() => {
+            navigate(`/hotel/${hotelId}`);
+          }}
+        />
+      </CardContent>
+    </Card>
+  );
+};
+
+export default RecentHotelCard;
