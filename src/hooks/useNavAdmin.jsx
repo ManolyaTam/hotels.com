8fe86326e86c73f7393a40bf3071474c8e04c0aa
Manolya Tamimi <tamimimanolya@gmail.com>
2024-01-07 18:17:38 +0200

    rendering hotels, rooms or cities depending on route (with static data for cities)

--- src/hooks/useNavAdmin.jsx
+++ src/hooks/useNavAdmin.jsx
@@ -2,7 +2,8 @@ import { useLocation, useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 
 const useNavAdmin = () => {
-  const [data, setData] = useState(); // array of cities, hotels or roomss
+  const [data, setData] = useState([]); // array of cities, hotels or roomss
+  const [dataType, setDataType] = useState("cities"); // "cities", "hotels" or "rooms"
   const location = useLocation();
   const navigate = useNavigate();
   const route = location.pathname;
@@ -11,17 +12,30 @@ const useNavAdmin = () => {
     switch (route) {
       case "/admin/cities": {
         // fetch cities and set data
-        setData("cities");
+        setDataType("cities");
+        setData([{
+          id: 8,
+          name: "Cape Town",
+          description: "Enjoy the breathtaking landscapes of Cape Town. From the iconic Table Mountain to pristine beaches, experience the natural beauty and cultural richness of this South African gem."
+        },
+        {
+          id: 4,
+          name: "Denver",
+          description: "Nestled at the foothills of the Rocky Mountains, Denver, the Mile-High City, is a vibrant metropolis known for its outdoor recreation, craft breweries, and cultural attractions. With a mix of modern skyscrapers and historic architecture, Denver offers a dynamic urban experience against the stunning backdrop of the nearby mountains, making it a gateway to both urban sophistication and natural beauty."
+        },
+        ]);
         break;
       }
       case "/admin/hotels": {
         // fetch hotels and set data
-        setData("hotes");
+        setDataType("hotels");
+        setData(null);
         break;
       }
       case "/admin/rooms": {
         //fetch rooms and set data
-        setData("rooms");
+        setDataType("rooms");
+        setData(null);
         break;
       }
       default: {
@@ -33,6 +47,6 @@ const useNavAdmin = () => {
     }
   }, [route, navigate]);
 
-  return { data };
+  return { data, dataType };
 };
 export default useNavAdmin;
--- src/pages/Admin/Admin.jsx
+++ src/pages/Admin/Admin.jsx
@@ -1,22 +1,35 @@
 import Input from "../../components/Input";
 import Button from "../../components/Button";
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import useNavAdmin from "../../hooks/useNavAdmin";
+import Cities from "./Cities";
 
 const Admin = () => {
-  const { data } = useNavAdmin();
+  const { data, dataType } = useNavAdmin();
   return (
     <Box marginInline="auto" maxWidth={1000}>
       {/* TODO: Search Form */}
       <Box display="flex">
-        <Input label={`Search ${data}`} type="text" fullWidth />
-        <Button label="Search" variant="contained" />
+        <Input label={`Search ${dataType}`} type="text" fullWidth />
+        <Button label="Search" variant="contained" style={{ marginInline: 5 }} />
+        <Button label="Create" variant="outlined" style={{ marginInline: 5 }} />
       </Box>
 
       {/* TODO:  City/Hotel/Room Create Form, activated by clicking create btn */}
-      <Button label="Create" variant="outlined" />
-
       {/* TODO: Update Form activated by clicking a row */}
+      {
+        data?.length
+          ? (
+            dataType === "cities"
+              ? <Cities data={data} />
+              : dataType === "hotels"
+                ? "<Hotels data={data} />"
+                : dataType === "rooms"
+                  ? "<Rooms data={data} />"
+                  : ""
+          )
+          : <Typography textAlign="center" marginBlock={5}>No results found...</Typography>
+      }
     </Box>
   );
 };
