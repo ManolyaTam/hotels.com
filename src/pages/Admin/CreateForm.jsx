d2c7d3a3cb69dfb971e5e28e539c525f0ef5754e
Manolya Tamimi <tamimimanolya@gmail.com>
2024-01-09 22:50:42 +0200

    TABP-49: Creating createForm sidebar component

---
+++ src/pages/Admin/CreateForm.jsx
@@ -0,0 +1,24 @@
+import { Box, Drawer, IconButton } from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
+
+const CreateForm = ({ children, isOpen, setIsOpen }) => {
+  return (
+    <>
+      <Drawer
+        open={isOpen}
+        onClose={() => setIsOpen(false)}
+        variant="persistent"
+        anchor="right"
+      >
+        <div>
+          <IconButton onClick={() => setIsOpen(false)}>
+            <CloseIcon />
+          </IconButton>
+        </div>
+        <Box sx={{ padding: 2, boxSizing: "border-box" }}>{children}</Box>
+      </Drawer>
+    </>
+  );
+};
+
+export default CreateForm;
