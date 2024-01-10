e7f31a5d3766447262d8cb25bd165feeead258d8
Manolya Tamimi <tamimimanolya@gmail.com>
2024-01-10 09:34:30 +0200

    TABP-54: removing cardActionArea from card components

--- src/components/DestinationCard.jsx
+++ src/components/DestinationCard.jsx
@@ -1,19 +1,9 @@
 import { Card, CardContent, CardMedia, Box, Typography } from "@mui/material";
-import { CardActionArea } from "@mui/material";
 
-const DestinationCard = ({
-  onClick,
-  imgUrl,
-  alt,
-  country,
-  city,
-  description,
-}) => {
+const DestinationCard = ({ imgUrl, alt, country, city, description }) => {
   return (
     <Card sx={{ maxWidth: 470 }}>
-      <CardActionArea onClick={onClick}>
-        <CardMedia component="img" height="280" image={imgUrl} alt={alt} />
-      </CardActionArea>
+      <CardMedia component="img" height="280" image={imgUrl} alt={alt} />
       <CardContent>
         <Typography variant="h5" color="text.primary">
           {city}, {country}
